1bdbf0c76a3bf5b509dc9d709b2e9eba128d26af
Caroline Gatwiri <gatwiri.caroline@eclectics.io>
2025-05-29 11:53:40 +0300

    Compound calculator in Java

--- day 2/CompoundCalculator.java
+++ day 2/CompoundCalculator.java
@@ -1,2 +1,39 @@
+import java.util.Scanner;
+
 public class CompoundCalculator {
+    public static void main(String[]arg){
+        Scanner scanner = new Scanner(System.in);
+        double fullAmount;//Final amount A
+        double principal;//Initial Amount of money P
+        double r;//Annual interest rate r
+        int n;//Number of times interest is compounded n
+        int t;//Number od years t
+        double compoundedInterest;
+
+
+
+        System.out.println("Enter the initial Amount of money: ");
+        principal = scanner.nextDouble();
+        System.out.println("Enter the annual interest rate: ");
+        double rate = scanner.nextDouble();
+        r = rate/100;
+        System.out.println("Enter Number of times interest is compounded: ");
+        n = scanner.nextInt();
+        System.out.println("Enter Number of years: ");
+        t = scanner.nextInt();
+
+        fullAmount = principal*Math.pow((1+(r/n)), n*t);
+        compoundedInterest = fullAmount - principal;
+
+        System.out.println("Your final amount is: "+fullAmount);
+        System.out.println("Your Compound interest is: "+compoundedInterest);
+
+
+
+
+
+
+        scanner.close();
+
+    }
 }
